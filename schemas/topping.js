73559028f60672437b0a66732d726f1dc29d10df
BrainJarProductions <vgrilli1@gmail.com>
2022-02-13 12:14:18 -0500

    fill out toppings schema

---
+++ schemas/topping.js
@@ -0,0 +1,36 @@
+import { FaPepperHot as icon, FaSeedling as leaf } from 'react-icons/fa';
+
+export default {
+  // computer name
+  name: 'topping',
+  // visabe name
+  title: 'Toppings',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Topping Name',
+      type: 'string',
+      description: 'Name of the topping',
+    },
+    {
+      name: 'vegetarian',
+      title: 'Vegetarian',
+      type: 'boolean',
+      description: 'Is this topping vegetarian?',
+      options: {
+        layout: 'checkbox',
+      },
+    },
+  ],
+  preview: {
+    select: {
+      name: 'name',
+      vegetarian: 'vegetarian',
+    },
+    prepare: (fields) => ({
+      title: `${fields.name} ${fields.vegetarian ? '🌱' : ''}`,
+    }),
+  },
+};
